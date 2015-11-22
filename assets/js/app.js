15eb986a7b32c5d1cfc7b1465f43c6620b1d45c2
Rajaie AlKorani <rajaie.alkorani@mail.utoronto.ca>
2015-11-22 15:58:59 -0500

    Improve site layout and add an homepage intro image. Add json loading to menus page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,60 +11,68 @@ myApp.config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
 
     $stateProvider
     .state('default', {
-      url: "/",
-      templateUrl: "views/home.html",
-      controller: 'SimpleController',
+        url: "/",
+        templateUrl: "views/home.html",
+        controller: 'SimpleController',
     })
     .state('register', {
-      url: "/register",
-      templateUrl: "views/register.html",
-      controller: 'SimpleController',
+        url: "/register",
+        templateUrl: "views/register.html",
+        controller: 'SimpleController',
     })
     .state('login', {
-      url: "/login",
-      templateUrl: "views/login.html",
-      controller: 'SimpleController',
+        url: "/login",
+        templateUrl: "views/login.html",
+        controller: 'SimpleController',
     })
     .state('orders', {
-      url: "/orders",
-      templateUrl: "views/orders.html",
-      controller: 'SimpleController',
+        url: "/orders",
+        templateUrl: "views/orders.html",
+        controller: 'SimpleController',
     })
     .state('payments', {
-      url: "/payments",
-      templateUrl: "views/payments.html",
-      controller: 'SimpleController',
+        url: "/payments",
+        templateUrl: "views/payments.html",
+        controller: 'SimpleController',
     })
     .state('profile', {
-      url: "/profile",
-      templateUrl: "views/profile.html",
-      controller: 'SimpleController',
+        url: "/profile",
+        templateUrl: "views/profile.html",
+        controller: 'SimpleController',
     })
     .state('profile.edit', {
-      url: "/edit",
-      templateUrl: "views/profile.edit.html",
-      controller: 'SimpleController',
+        url: "/edit",
+        templateUrl: "views/profile.edit.html",
+        controller: 'SimpleController',
     })
     .state('menus', {
-      url: "/menus",
-      abstract: true,
-      templateUrl: "views/menus.html",
-      resolve: {
+        url: "/menus",
+        abstract: true,
+        templateUrl: "views/menus.html",
+        resolve: {
             menus: function(menusFactory){
                 return menusFactory.all();
             }
         },
-      controller: 'MenusController',
+        controller: 'MenusController',
     })
     .state('menus.list', {
-      url: "",
-      templateUrl: "views/menus.list.html",
-      controller: 'MenusController',
+        url: "",
+        templateUrl: "views/menus.list.html",
     })
-    .state('menus.detail', {
-      url: "/detail",
-      templateUrl: "views/menus.detail.html",
-      controller: 'MenusController',
+    .state('menus.list.detail', {
+        url: "/:id",
+        templateUrl: "views/menus.list.detail.html",
+        controller: function ($scope, $stateParams) {
+            menuId = $stateParams.id;
+            menus = $scope.menus;
+            for (var i = 0; i < menus.length; i++) {
+                if (menus[i].id == menuId) {
+                    $scope.menu = menus[i];
+                    break;
+                }
+            }
+        }
     })
     .state('messages', {
       url: "/messages",
--- assets/js/factories.js
+++ assets/js/factories.js
@@ -2,7 +2,6 @@
 myApp.factory('menusFactory', function ($http) {
   var path = 'assets/json/menus.json';
   var menus = $http.get(path).then(function (resp) {
-    console.log(resp.data.menus);
     return resp.data.menus;
   });
 
